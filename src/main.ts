8431268744da1670c86409eb4a367673318bf3a5
dastin <astindillon@gmail.com>
2025-10-20 00:26:54 -0400

    fixed sprite transparency

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import kaplay from "kaplay";
 const k = kaplay();
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/bean.png");
+k.loadSprite("bean", "sprites/golf2.webp", {
+  sliceX: 12,
+  sliceY: 12,
+  anims: {
+    idle: {from: 6, to: 7}
+  }
+});
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
+const player = k.add([k.pos(0,0), k.sprite("bean")]);
+player.play("idle");
 
 k.onClick(() => k.addKaboom(k.mousePos()));
